a0160b3c23607ea34206751368cce6720b4bf0d4
AleksandarRatkov <alexandar.ratkov@gmail.com>
2016-06-20 16:10:34 +0200

    Inital commit UDD

---
+++ UDD_Project/src/main/java/model/NewspaperArticle.java
@@ -0,0 +1,14 @@
+package model;
+
+/**
+ * Created by Aleksandar Ratkov on 20.6.16..
+ */
+public class NewspaperArticle {
+
+    private Long id;
+    private String title;
+    private String apstract;
+
+
+
+}
